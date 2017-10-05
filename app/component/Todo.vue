75c57e11f297f2bd67dde43fc9cba68bab25712a
Spencer Bouse <Orangesrb@gmail.com>
2017-10-05 07:11:11 -0400

    todo w/ jquery

--- app/component/Todo.vue
+++ app/component/Todo.vue
@@ -14,24 +14,31 @@
       </div>
     </div>
     <div v-if="renderCreateTodo" class="animated fadeIn">
-      <CreateTodo @clicked="onload"></CreateTodo>
+      <div>
+        <p class="new-todo-text">New Todo</p>
+        <div class="large-border">
+          <div class="small-border">
+            <img class="input-arrow" src="/images/arrow.png" alt='arrow'>
+            <input type="text" class="todo-input" placeholder="Add New Todo" v-model='newTodo' @keyup.enter="addTodo">
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import CreateTodo from './create-todo.vue'
+import $ from 'jquery';
 
 export default {
   data(){
     return {
       renderCreateTodo: true,
       todos: '',
-      square: '■'
+      square: '■',
+      newTodo: '',
+      todos: ''
     }
-  },
-  components:{
-    CreateTodo: CreateTodo,
   },
   methods:{
     onload: function(){
@@ -39,6 +46,34 @@ export default {
     },
     updateTodo: function(todo){
       this.todos.push(todo)
+    },
+    addTodo: function(){
+      var value = this.newTodo
+      if (!value){
+        return
+      }
+      var id = Date.now()
+      var todos = []
+      var todo ={
+        id: id,
+        name: value,
+        edit: false
+      }
+      if(!this.todos){
+        todos.push(todo)
+        localStorage.setItem("todos", JSON.stringify(todos));
+      }else{
+        this.todos.push(todo)
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+        this.newTodo = ''
+      }
+      $('.todo-figure').append(
+      '<div style="padding: 0" class="fadeInLeft">\n'+
+        '<button style="margin-top: 0px;" type="button" class="custom-button">\n'+
+          '<p><span>&#9632</span>'+todo.name+'</p>\n'+
+          '<img class="arrow animated fadeIn" src="/images/arrow.png" alt="arrow">\n'+
+        '</button>\n'+
+      '</div>')
     }
   },
   beforeMount(){
@@ -59,5 +94,19 @@ export default {
   font-size: 27px;
   background-color: #454138;
   cursor: pointer;
+}
+.new-todo-text{
+  font-size: 17px;
+  margin-bottom: 5px;
+}
+.input-arrow{
+  position: absolute;
+  height: 30px;
+  margin-left:-20px;
+}
+.todo-input{
+  margin-left: 20px;
+  width: 87%;
+  box-shadow: 3px 3px 5px #888888;
 }
 </style>
